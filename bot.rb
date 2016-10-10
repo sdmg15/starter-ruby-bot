4c72c46684a1c6b1917bd064e781f12ce12bfb0e
Sonkeng Maldini <sdmg15@users.noreply.github.com>
2016-10-10 11:45:54 +0100

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -21,7 +21,7 @@ end
 
 #mes tests 
 client.on :user_typing do |data| 
-  client.message channel: data['channel'], text: " Welcome <@#{data['user']}> here's the guide ! \n Bienvenue <@#{data['user']}> voici le guide \nhttps://devoffuture.slack.com/files/sdmg15/F2HVBAZJR/Guide_of_new_user \nhttps://devoffuture.slack.com/files/sdmg15/F2G00CFM3/Guide_du_nouveau" 
+  client.message channel: data['channel'], text: " Welcome <@#{data['user']}> here's the guide ! https://devoffuture.slack.com/files/sdmg15/F2HVBAZJR/Guide_of_new_user \nBienvenue <@#{data['user']}> voici le guide https://devoffuture.slack.com/files/sdmg15/F2G00CFM3/Guide_du_nouveau" 
 end  
 # listen for channel_joined event - https://api.slack.com/events/channel_joined
 client.on :channel_joined do |data|
