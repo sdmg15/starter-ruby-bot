375364fb960dd5c8c47d8050eb03e568325b28b0
Sonkeng Maldini <sdmg15@users.noreply.github.com>
2016-10-10 09:42:26 +0100

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -21,7 +21,7 @@ end
 
 #mes test 
 client.on :user_typing do |data| 
-  client.message channel: data['channel'] , text: " Identifiant de la team #{data['team_id']} Réaction ajoutée par <@#{data['user']}> la réaction est #{data['reaction']}" 
+  client.message channel: data['channel'], text: "Identifiant de la team #{data['team_id']} Réaction ajoutée par <@#{data['user']}>" 
 end 
 # listen for channel_joined event - https://api.slack.com/events/channel_joined
 client.on :channel_joined do |data|
