ce6199c39c73ccbe107948579fb601ae83afd2cb
Sonkeng Maldini <sdmg15@users.noreply.github.com>
2016-10-10 12:25:29 +0100

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -23,7 +23,9 @@ end
 client.on :user_typing do |data| 
   if data['channel'] == 'general'
     client.message data['channel'], text: "Vous êtes dans généra"
-  end
+  else 
+    clien.message data['channel'], text: "Vous netes pas dans #general"
+  end 
 end  
 # listen for channel_joined event - https://api.slack.com/events/channel_joined
 client.on :channel_joined do |data|
