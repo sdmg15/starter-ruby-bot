3a739a2b25d027f8e19149f3ec69d7287cb78586
Sonkeng Maldini <sdmg15@users.noreply.github.com>
2016-10-10 12:44:17 +0100

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -20,7 +20,9 @@ client.on :hello do
 end
 
 #mes tests 
- 
+client.on :user_typing do |data| 
+  client.message channel: data['channel'], text: "Vous etes ici : #{data['channel']} "
+end 
 # listen for channel_joined event - https://api.slack.com/events/channel_joined
 client.on :channel_joined do |data|
   if joiner_is_bot?(client, data)
