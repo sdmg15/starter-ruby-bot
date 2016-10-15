421cc50470cd22df546d998f848c4e42a1a2e06f
Sonkeng Maldini <sdmg15@users.noreply.github.com>
2016-10-15 15:28:53 +0100

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -22,10 +22,10 @@ end
 #mes tests 
 client.on :channel_joined do |data|
   if joiner_is_bot?(client, data)
-    logger.debug("Someone far less important than #{client.self['name']} joined #{data['channel']['id']}")
-  else
     client.message channel: data['channel']['id'], text: "Thanks for the invite! I don\'t do much yet, but #{help}"
     logger.debug("#{client.self['name']} joined channel #{data['channel']['id']}")
+  else
+    logger.debug("Someone far less important than #{client.self['name']} joined #{data['channel']['id']}")
   end
 end
 # listen for channel_joined event - https://api.slack.com/events/channel_joined
